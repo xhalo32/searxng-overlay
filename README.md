b93247284d9b90371c1c237d9f00af1bd89a57f6
Niklas Halonen <niklas.halonen@protonmail.com>
2026-05-19 13:58:30 +0300

    init: add searxng-static and searxng

---
+++ README.md
@@ -0,0 +1,41 @@
+# SearXNG priv.au Nix Overlay
+
+This repository is a Nix port of [privau/searxng](https://github.com/privau/searxng), a [SearXNG](https://github.com/searxng/searxng) "fork" with themes and other features.
+
+In particular, this overlay provides various themes to SearXNG through `Preferences > User Interface > Theme style`.
+
+This overlay also acts as a future reference for customizing SearXNG as the [How To Add Theme On SearXNG](https://github.com/searxng/searxng/discussions/3707) discussion doesn't provide any useful insight into how to customize and rebuild the stylesheets.
+
+## Usage
+
+Inputs (npins sources):
+- `nixpkgs` (follows `nixos-unstable`)
+- `searxng` (follows `searxng/searxng` master)
+- `privau-searxng` (follows `privau/searxng` main)
+
+To add this repository as an overlay, first add it as a source
+
+```
+npins add github xhalo32 searxng-overlay --branch main
+```
+
+To add the overlay to your pkgs with the (possibly outdated) sources provided by this repository, use
+
+```nix
+(final: prev: {
+    inherit (import sources.searxng-overlay { }) searxng;
+})
+```
+
+> If you want to inject your own dependencies, and your sources already contain `nixpkgs`, `searxng` and `privau-searxng`, use
+> 
+> ```nix
+> (final: prev: {
+>     inherit (import sources.searxng-overlay { inherit sources; }) searxng;
+> })
+> ```
+
+## Background
+
+To figure out how to rebuild the stylesheets I read throgh privau's [update.sh](https://github.com/privau/searxng/blob/d81eb1fba5285c3459067da504813571a2592d24/update.sh) and [Dockerfile](https://github.com/privau/searxng/blob/d81eb1fba5285c3459067da504813571a2592d24/Dockerfile).
+The nix build runs the critical steps in these scripts.
---
+++ default.nix
@@ -0,0 +1,49 @@
+{
+  sources ? import ./npins,
+  system ? builtins.currentSystem,
+  pkgs ? import sources.nixpkgs {
+    inherit system;
+    config = { };
+    overlays = [ ];
+  },
+}:
+rec {
+  searxng-static = pkgs.buildNpmPackage (finalAttrs: {
+    pname = "searxng-client";
+    version = "master";
+    src = sources.searxng;
+    npmRoot = "client/simple";
+    # npmFlags = [ "--loglevel=verbose" ];
+    # npmDeps = pkgs.importNpmLock { npmRoot = "${finalAttrs.src}/client/simple"; };
+    npmDepsHash = "sha256-kSx2IvAMKHNoZeS1Tac1haDPKYz/yHUND1boqT4Fbto=";
+    # https://github.com/privau/searxng/blob/main/update.sh
+    prePatch = ''
+      cp -r ${sources.privau-searxng}/src/less/. client/simple/src/less
+    '';
+    # Why is this needed when `npmRoot` is set?
+    preBuild = ''
+      cd client/simple
+    '';
+    installPhase = ''
+      mkdir $out
+      cp -r ../../searx/static/themes/simple/. $out
+    '';
+
+    meta = {
+      license = pkgs.lib.licenses.agpl3Only;
+    };
+  });
+
+  searxng = pkgs.searxng.overrideAttrs (oa: {
+    src = sources.searxng;
+    prePatch = ''
+      cp -r ${searxng-static}/. searx/static/themes/simple/
+      substituteInPlace searx/preferences.py \
+        --replace-fail "'auto', 'light', 'dark', 'black']" "'auto', 'light', 'dark', 'black', 'paulgo', 'latte', 'frappe', 'macchiato', 'mocha', 'kagi', 'brave', 'moa', 'night', 'dracula', 'gruvbox', 'gruvboxmat', 'everforest', 'nord', 'matcha']"
+      substituteInPlace searx/settings_defaults.py \
+        --replace-fail "SIMPLE_STYLE = ('auto', 'light', 'dark', 'black')" "SIMPLE_STYLE = ('auto', 'light', 'dark', 'black', 'paulgo', 'latte', 'frappe', 'macchiato', 'mocha', 'kagi', 'brave', 'moa', 'night', 'dracula', 'gruvbox', 'gruvboxmat', 'everforest', 'nord', 'matcha')"
+      substituteInPlace searx/templates/simple/preferences/theme.html \
+        --replace-fail "{%- for name in ['auto', 'light', 'dark', 'black'] -%}" "{%- for name in ['auto', 'light', 'dark', 'black', 'paulgo', 'latte', 'frappe', 'macchiato', 'mocha', 'kagi', 'brave', 'moa', 'night', 'dracula', 'gruvbox', 'gruvboxmat', 'everforest', 'nord', 'matcha'] -%}"
+    '';
+  });
+}
